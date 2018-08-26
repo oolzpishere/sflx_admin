64c48ce3add617c81e6bcf7f6de3d8d9c8ff5eb3
leeli <leeli@leelis-iMac-2.local>
2018-08-26 17:24:11 +0800

    add image .svg etc.

--- db/migrate/20170608054740_create_images.rb
+++ db/migrate/20170608054740_create_images.rb
@@ -3,11 +3,11 @@ class CreateImages < ActiveRecord::Migration[5.0]
     create_table :images do |t|
       t.belongs_to :gallery, index: true
       t.integer :position
-      t.attachment :image
-      
+      # t.attachment :image
+
       t.timestamps
     end
 
-    
+
   end
 end
