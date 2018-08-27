e1cac2b1fe4cfd7aec00032d098a9068f04dcfcc
leeli <leeli@leelis-iMac-2.local>
2018-08-27 14:44:57 +0800

    link storage to shared

--- config/deploy.rb
+++ config/deploy.rb
@@ -82,4 +82,4 @@ set :keep_assets, 2
 # append :linked_files, "config/database.yml", "config/secrets.yml"
 
 # Default value for linked_dirs is []
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", 'public/uploads', 'public/assets'
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", 'public/uploads', 'public/assets', 'storage'
