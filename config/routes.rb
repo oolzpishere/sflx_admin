692d0dd4475dc773f1ddd1eef8ad8acf71621b90
leeli <leeli@leelis-iMac.local>
2018-08-28 02:11:10 +0800

    change device_for and crequest routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,12 +8,14 @@ Rails.application.routes.draw do
     root to: "users#index"
   end
 
-  devise_for :users
+  devise_for :users, only: :sessions
+  # devise_for :users, skip: [:registrations, :confirmations]
+
   resources :pages
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "pages#index"
   resources :galleries
-  resources :crequests
+  resources :crequests, only: [:new, :create]
 
   get 'conference', 'web', 'about', 'contact', 'contact/find_us', to: 'pages#show'
 
