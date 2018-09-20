3385ee4b7066247eeca489c1d120d4bf52202813
leeli <leeli@leelis-iMac-2.local>
2018-09-20 17:50:18 +0800

    limit routes of pages and galleries

--- config/routes.rb
+++ config/routes.rb
@@ -11,10 +11,10 @@ Rails.application.routes.draw do
   devise_for :users, only: :sessions
   # devise_for :users, skip: [:registrations, :confirmations]
 
-  resources :pages
+  resources :pages, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "pages#index"
-  resources :galleries
+  resources :galleries, only: [:index, :show]
   resources :crequests, only: [:new, :create]
 
   get 'conference', 'web', 'about', 'contact', 'contact/find_us', to: 'pages#show'
