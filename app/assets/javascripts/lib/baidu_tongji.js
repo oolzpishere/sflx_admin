be7d3a503d9faab5320778e82f81aa6a6db71599
leeli <leeli@leelis-iMac.local>
2018-11-04 10:36:05 +0800

    change baidu tongji

--- app/assets/javascripts/lib/baidu_tongji.js
+++ app/assets/javascripts/lib/baidu_tongji.js
@@ -2,7 +2,7 @@
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?d7d81cc43af9d972607d661f40b1c49c";
+  hm.src = "https://hm.baidu.com/hm.js?2f2025d3c049580358da7f7451789540";
   var s = document.getElementsByTagName("script")[0];
   s.parentNode.insertBefore(hm, s);
 })();
