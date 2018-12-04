eeeb0cf67b653ddf29045404cfb62a230d8cacaf
leeli <leeli@leelis-iMac.local>
2018-12-04 22:33:20 +0800

    add basic divise authenticate to sidekiq dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   require 'sidekiq/web'
-  mount Sidekiq::Web => "sidekiq"
+  authenticate :user do
+    mount Sidekiq::Web => "/sidekiq"
+  end
 
   namespace :admin do
     resources :users
