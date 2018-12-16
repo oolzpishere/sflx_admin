9749b77754087e073167a1aa9b748acb7f443d12
leeli <leeli@leelis-iMac.local>
2018-12-17 01:05:22 +0800

    add services explain to index

--- app/mailers/customer_mailer.rb
+++ app/mailers/customer_mailer.rb
@@ -3,6 +3,6 @@ class CustomerMailer < ApplicationMailer
   def crequest(attributes)
     @attributes = attributes
     mail(to: "oolzpishere@126.com",
-      subject: "sflx.com.cn:#{attributes['name']}发来新需求")
+      subject: "sflx.com.cn: \'#{attributes['name']}\'发来新需求")
   end
 end
