431e03561436460ac1b12fbd8335737e7f6cbd96
leeli <leeli@leelis-iMac.local>
2019-06-01 16:24:44 +0800

    delete app/assets redundants files

--- app/assets/javascripts/lib/baidu_tongji.js
+++
@@ -1,8 +0,0 @@
-
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?2f2025d3c049580358da7f7451789540";
-  var s = document.getElementsByTagName("script")[0];
-  s.parentNode.insertBefore(hm, s);
-})();
--- spec/support/files_test_helper.rb
+++ spec/support/files_test_helper.rb
@@ -1,6 +1,6 @@
 module FilesTestHelper
   extend self
-  extend ActionDispatch::TestProcess
+  # extend ActionDispatch::TestProcess
 
   def png_name; 'test-image.png' end
   def png; upload(png_name, 'image/png') end
@@ -18,7 +18,8 @@ module FilesTestHelper
 
   def upload(name, type)
     file_path = Rails.root.join('spec', 'support', 'assets', name)
-    fixture_file_upload(file_path, type)
+    Rack::Test::UploadedFile.new(file_path, type)
+    # fixture_file_upload(file_path, type)
   end
 
 end
